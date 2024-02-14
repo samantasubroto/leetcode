d5dd70e49c1719847149c46647e7d420e01d8d29
Subrotho Samantha <ssubrotho@kaartech.com>
2024-02-14 10:11:57 +0530

    some problems on sliding window

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String name = "subrotho";
         char[] arr = name.toCharArray();
-
+        Arrays.sort(arr);
+        int max = Integer.MIN_VALUE;
+        char previous_value = arr[0];
+        int count = 1;
+        System.out.println(arr);
+        for(int i=1;i<arr.length;i++) {
+            if(previous_value == arr[i]){
+                count += 1;
+                max = Math.max(count,max);
+            } else {
+                max = Math.max(count,max);
+                previous_value = arr[i];
+                count = 1;
+            }
+        }
+        System.out.println(max);
     }
 }
--- medium/slidingwindow/MinimumSizeSubarray.java
+++ medium/slidingwindow/MinimumSizeSubarray.java
@@ -18,7 +18,26 @@ public class MinimumSizeSubarray {
         return 0;
     }
 
+    public static int usingSlidingWindow(int target, int[] nums) {
+        int sum = 0;
+        int i=0,j = 0;
+        int result = nums.length+1;
+        while(j<nums.length) {
+            sum += nums[j];
+            while(sum >= target) //for sliding window we always use while
+            {
+                int len = j-i+1;
+                result = Math.min(result, len);
+                sum-= nums[i]; //removing the first element to check if its still satisfy the condition
+                i++;
+            }
+            j++;
+        }
+        return (result==nums.length+1) ? 0 : result;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(minSubArrayLen(213, new int[]{12,28,83,4,25,26,25,2,25,25,25,12}));
+        System.out.println(usingSlidingWindow(213, new int[]{12,28,83,4,25,26,25,2,25,25,25,12}));
     }
 }
