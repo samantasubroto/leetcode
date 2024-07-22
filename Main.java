c8a43cf38718cf042a767f33446474469bc8f652
Subrotho Samantha <ssubrotho@kaartech.com>
2024-07-22 19:54:01 +0530

    save lines

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
 
 class Main {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i=0;i<accounts.length;i++) {
+            int value = 0;
+            for(int j=0;j<accounts[i].length;j++) {
+                value += accounts[i][j];
+            }
+            max = Math.max(max, value);
+        }
+        return max;
+    }
     public static void main(String[] args) {
 
         String name = "hello";
